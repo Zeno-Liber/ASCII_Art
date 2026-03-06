cb629b96d7ca653d4757a880d87bab9b752396db
Zeno-Liber <zenoliber@outlook.com>
2026-03-06 13:09:11 -0600

    Create ascii_art.py

---
+++ ascii_art.py
@@ -0,0 +1,63 @@
+# Zeno Liber
+# Fun fact: I am a solutions summoner, I conjure up solutions to problems.
+
+first_initial = """
+Z Z Z Z Z
+        Z
+      Z
+    Z
+  Z
+Z
+Z Z Z Z Z
+"""
+
+second_initial = """
+L       
+L
+L
+L
+L
+L
+L L L L L
+"""
+
+# optional task of writing the full last name
+
+after_second_initial = """
+I I I I I
+    I
+    I
+    I
+    I
+    I
+I I I I I
+
+B B B B  
+B      B
+B      B
+B B B B
+B      B
+B      B
+B B B B
+
+E E E E E
+E
+E
+E E E
+E
+E
+E E E E E
+
+R R R R  
+R      R
+R      R
+R R R R
+R  R
+R    R
+R      R
+"""
+
+
+print(first_initial)
+print(second_initial)
+print(after_second_initial)
